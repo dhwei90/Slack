3078a1ce8a737335833e6051e6e54dfad9bc45d6
jerrywdh <billywdh@hotmail.com>
2021-03-21 22:36:50 -0700

    added files

--- slack.js
+++ slack.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const socketio = require('socket.io')
 
 let namespaces = require('./data/namespaces');
@@ -9,6 +10,9 @@ const port = process.env.PORT || 9000;
 const expressServer = app.listen(port);
 const io = socketio(expressServer);
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/chat.html'))
+})
 
 // io.on = io.of('/').on = io.sockets.on
 // io.emit = io.of('/').emit = io.sockets.emit
